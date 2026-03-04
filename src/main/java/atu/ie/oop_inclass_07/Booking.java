0e09e3ab76144e2d9832b7ad5c90c430100000d4
SamiHammar-STUDENT <awesomedude597sami@gmail.com>
2026-03-04 17:03:32 +0000

    Set Up

---
+++ src/main/java/atu/ie/oop_inclass_07/Booking.java
@@ -0,0 +1,16 @@
+package atu.ie.oop_inclass_07;
+import io.swagger.v3.oas.annotations.media.schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+
+
+
+
+public class Booking {
+}
